111bdc2fc45cd227334343f1e930dd8910796d72
s.kosov <s.kosov@whatwhere.world>
2019-07-05 15:10:04 +0400

    Add fonts and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import RootRoutes from './RootRoutes'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { theme, GlobalStyle } from 'services/theme'
 
 const App: React.FC = () => {
   return (
     <Router>
-      <RootRoutes />
+      <ThemeProvider theme={theme}>
+        <Fragment>
+          <GlobalStyle />
+          <RootRoutes />
+        </Fragment>
+      </ThemeProvider>
     </Router>
   )
 }
